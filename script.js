491b8b961aaec784614477e0e9a8360119de74ef
Nogiback <peter.toan.do@gmail.com>
2023-08-28 17:36:27 -0400

    Added modal HTML structure for adding books, added some script for book/library functions

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let library = JSON.parse(localStorage.getItem("books")) || [];
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
