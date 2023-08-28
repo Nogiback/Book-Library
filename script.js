fc24c35b2b486f1802c40eee3fbf9500b9264fcd
Nogiback <peter.toan.do@gmail.com>
2023-08-28 18:07:49 -0400

    Added modal to HTML, added script to show modal upon button click

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const addBookBtn = document.getElementById('add-book');
+const addBookDialog = document.getElementById('container');
+
+addBookBtn.addEventListener('click', () => {
+  addBookDialog.showModal();
+});
+
 let library = JSON.parse(localStorage.getItem("books")) || [];
 
 class Book {
